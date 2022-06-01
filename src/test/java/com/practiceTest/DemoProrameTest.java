fc0f2631547337f821f5bc73704ab372d614a0c6
bhushana <bhushanamadhu@gmail.com>
2022-06-01 12:32:09 +0530

    first commit

---
+++ src/test/java/com/practiceTest/DemoProrameTest.java
@@ -0,0 +1,15 @@
+package com.practiceTest;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class DemoProrameTest {
+	
+	@Test
+	public void practiceTest() {
+	Reporter.log("GIT CMD BATCH",true);
+		
+	}
+	
+
+}
